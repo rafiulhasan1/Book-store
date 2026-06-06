c5064dc1774e525cb499f26892470b77e11c0564
rafiulhasan1 <rafiulhasan001@gmail.com>
2026-06-06 16:51:57 +0600

    final update and bug fixes

--- src/NavBer/NavBer.jsx
+++ src/NavBer/NavBer.jsx
@@ -20,7 +20,7 @@ const NavBer = () => {
                         {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Boi-Poka</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
